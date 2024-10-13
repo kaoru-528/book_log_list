197f5f18f87f20e24fc4d2d42d7f853cf0443230
kaoru-528 <origami.kaoru@gmail.com>
2024-10-14 00:32:09 +0900

    メール認証機能の廃止

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,8 +14,8 @@ class User < ApplicationRecord
     has_many :followers, through: :passive_relationships, source: :follower
 
     attr_accessor :remember_token, :activation_token, :reset_token
-    before_save :downcase_email
-    before_create :create_activation_digest
+    # before_save :downcase_email
+    # before_create :create_activation_digest
     validates :name, presence: true, length: { maximum: 50 }
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
     validates :email, presence: true, length: { maximum: 255 },
@@ -96,12 +96,12 @@ class User < ApplicationRecord
     end
 
     private
-        def downcase_email
-            email.downcase!
-        end
+        # def downcase_email
+        #     email.downcase!
+        # end
 
-        def create_activation_digest
-            self.activation_token = User.new_token
-            self.activation_digest = User.digest(activation_token)
-        end
+        # def create_activation_digest
+        #     self.activation_token = User.new_token
+        #     self.activation_digest = User.digest(activation_token)
+        # end
 end
