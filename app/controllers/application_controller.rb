7e51ca2f00537ea3ff7156179a32988b9ead7252
kaoru-528 <origami.kaoru@gmail.com>
2024-10-25 23:59:19 +0900

    update:gitignore

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+class ApplicationController < ActionController::Base
+  include SessionsHelper
+
+  private
+
+  def logged_in_user
+    return if logged_in?
+
+    store_location
+    flash[:danger] = 'Please log in.'
+    redirect_to login_url, status: :see_other
+  end
+end
