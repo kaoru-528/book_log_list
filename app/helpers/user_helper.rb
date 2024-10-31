de5cf14a65752ef99819792292769feb4cbbc3c1
kaoru-528 <origami.kaoru@gmail.com>
2024-10-31 23:40:26 +0900

    update:軽微な修正

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,12 +1,6 @@
 # frozen_string_literal: true
 
 module UserHelper
-  # def gravatar_for(user, options = {size:80})
-  #     size = options[:size]
-  #     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-  #     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
-  #     image_tag(gravatar_url, alt: user.name, class: "gravatar")
-  # end
   def check_anonymous(user)
     if user.anonymous && !current_user?(user)
       user.name = 'Anymouse'
